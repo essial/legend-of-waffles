0d1809b8348171b20603752d2024d56fd0acecea
Tim Sarbin <tim.sarbin@gmail.com>
2013-01-15 23:45:07 -0500

    More updates and optimizations to remove duplicate code.

---
+++ Main/src/com/lunaticedit/legendofwaffles/base/ActorBase.java
@@ -0,0 +1,11 @@
+package com.lunaticedit.legendofwaffles.base;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timsarbin
+ * Date: 1/15/13
+ * Time: 10:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ActorBase {
+}
